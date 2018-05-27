4a01a8e07dc2d5a6439a3e9b40f5ee8c34e08cc3
Tobias Siemonsen <tobias@siemonsen.com>
2018-05-27 21:07:22 +0200

    Added Music Component

--- Amy-Webapp/src/app/app.module.ts
+++ Amy-Webapp/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MusicComponent } from './Components/music/music.component';
+import { WeatherComponent } from './Components/weather/weather.component';
+import { ClockComponent } from './Components/clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicComponent,
+    WeatherComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule
