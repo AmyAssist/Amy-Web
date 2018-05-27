e784c541acade85dcaf3f4b587118f263e124f1a
Tobias Siemonsen <tobias@siemonsen.com>
2018-05-27 21:07:50 +0200

    Added Clock Component

---
+++ Amy-Webapp/src/app/Components/clock/clock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
