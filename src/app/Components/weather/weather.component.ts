ff6fc7fc77910b0420c30e3304142f36ef75a9d6
Tobias Siemonsen <tobias@siemonsen.com>
2018-06-04 07:56:30 +0200

    Implemented Web UI for weather component close AmyAssist/Amy#13

--- src/app/Components/weather/weather.component.ts
+++ src/app/Components/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { DatabaseService } from '../../Services/database.service';
 import { weather } from '../../Objects/weather'
 
@@ -12,6 +13,9 @@ export class WeatherComponent implements OnInit {
   weatherToday: weather;
   weatherTomorrow: weather;
   weatherWeek: weather[];
+  today: boolean;
+  tommorow: boolean;
+  week: boolean;
 
   constructor(private databaseService: DatabaseService) { }
 
@@ -19,19 +23,31 @@ export class WeatherComponent implements OnInit {
     this.weatherToday = new weather();
     this.weatherTomorrow = new weather();
     this.weatherWeek = new Array<weather>();
+    this.today = false;
+    this.tommorow = false;
+    this.week = false;
   }
 
   getWeatherToday() {
+    this.today = true;
+    this.tommorow = false;
+    this.week = false;
     this.databaseService.getWeatherToday()
     .subscribe((data : weather) => this.weatherToday = { ...data});
   }
 
   getWeatherTomorrow() {
+    this.today = false;
+    this.tommorow = true;
+    this.week = false;
     this.databaseService.getWeatherTomorrow()
     .subscribe((data : weather) => this.weatherTomorrow = { ...data});
   }
 
   getWeatherWeek() {
+    this.today = false;
+    this.tommorow = false;
+    this.week = true;
     this.databaseService.getWeatherWeek()
     .subscribe((data : weather[]) => this.weatherWeek = { ...data});
   }
