8ee1a03c9d4af61c1da452a7990d98e571599d92
Tobias Siemonsen <tobias@siemonsen.com>
2018-06-04 07:56:00 +0200

    Implemented Web UI for music component close AmyAssist/Amy#28

--- src/app/Components/music/music.component.ts
+++ src/app/Components/music/music.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { DatabaseService } from '../../Services/database.service';
 import { music } from '../../Objects/music'
 
@@ -11,25 +12,37 @@ export class MusicComponent implements OnInit {
 
   musicData: music;
   musicPlaylistData: music[];
+  song: boolean;
+  playlist: boolean;
   
   constructor(private databaseService: DatabaseService) { }
 
   ngOnInit() {
+    this.song = false;
+    this.playlist = false;
     this.musicData = new music;
     this.musicPlaylistData = new Array<music>();
   }
 
   getCurrentSong() {
+    this.song = true;
+    this.playlist = false;
     this.databaseService.getCurrentSong()
     .subscribe((data : music) => this.musicData = { ...data});
   }
 
   getPlaylist() {
+    this.song = false;
+    this.playlist = true;
     this.databaseService.getPlaylist()
     .subscribe((data : music[]) => this.musicPlaylistData = { ...data});
   }
 
-  /*
-  playSong(){}
-  */
+  playSong(artist: string, titel: string) {
+    this.musicData.artist = artist;
+    this.musicData.titel = titel;
+    this.databaseService.playSong(this.musicData);
+    this.song = true;
+    this.playlist = false;
+  }
 }
