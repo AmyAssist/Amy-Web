8ac21dc56b1c7334af4bef5a42d54a98c5a370f9
Tobias Siemonsen <tobias@siemonsen.com>
2018-06-07 14:21:21 +0200

    Fixed lower case in database.service.ts

--- src/app/Services/database.service.ts
+++ src/app/Services/database.service.ts
@@ -29,49 +29,49 @@ export class DatabaseService {
     Getting all Alarms.
   */
   getAlarms() {
-    return this.http.get<clock[]>('http://localhost:8080/rest/Alarms', httpOptions);
+    return this.http.get<clock[]>('http://localhost:8080/rest/alarms');
   }
 
   /*
     Getting the current Song.
   */
   getCurrentSong() {
-    return this.http.get<music>('http://localhost:8080/rest/music/currentSong', httpOptions);
+    return this.http.get<music>('http://localhost:8080/rest/music/currentSong');
   }
 
   /*
     Getting the current Playlist.
   */
   getPlaylist() {
-  return this.http.get<music[]>('http://localhost:8080/rest/music/playlist', httpOptions);
+  return this.http.get<music[]>('http://localhost:8080/rest/music/playlist');
   }
 
   /*
     giving a command to play a song.
   */
   playSong(musicData: music){
-    this.http.post<music>('http://localhost:8080/rest/music/play', musicData, httpOptions);
+    this.http.post<music>('http://localhost:8080/rest/music/play', musicData);
   }
 
   /*
     getting weather-Data for today.
   */
   getWeatherToday() {
-    return this.http.get<weather>('http://localhost:8080/rest/weather/Today', httpOptions);
+    return this.http.get<weather>('http://localhost:8080/rest/weather/today');
   }
 
   /*
     getting weather-Data for tomorrow.
   */
   getWeatherTomorrow() {
-  return this.http.get<weather>('http://localhost:8080/rest/weather/Tomorrow', httpOptions);
+  return this.http.get<weather>('http://localhost:8080/rest/weather/tomorrow');
   }
 
   /*
     getting weather-Data for the whole week.
   */
   getWeatherWeek() {
-  return this.http.get<weather[]>('http://localhost:8080/rest/weather/Week', httpOptions);
+  return this.http.get<weather[]>('http://localhost:8080/rest/weather/week');
   }
 }
 
