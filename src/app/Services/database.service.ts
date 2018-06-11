6e773d6d00644c36c735dd6163aa6764adc1d6a2
Tobias Siemonsen <tobias@siemonsen.com>
2018-06-11 13:18:30 +0200

    path changed to variable

--- src/app/Services/database.service.ts
+++ src/app/Services/database.service.ts
@@ -17,7 +17,11 @@ import { weatherWeek } from '../Objects/weatherWeek';
 })
 export class DatabaseService {
 
-  constructor(private http: HttpClient) { }
+  path: string;
+
+  constructor(private http: HttpClient) { 
+    this.path = 'http://localhost:8080/rest/';
+  }
 
   /*
     Getting the current command for the web application.
@@ -31,56 +35,56 @@ export class DatabaseService {
     Getting all Alarms.
   */
   getAlarms() {
-    return this.http.get<clock[]>('http://localhost:8080/rest/clock/alarms');
+    return this.http.get<clock[]>(this.path + 'clock/alarms');
   }
 
   /*
     Creating a new Alarm.
   */
   setNewAlarm(clockData: clock) {
-    this.http.post<clock>('http://localhost:8080/rest/clock/alarms/new', clockData);
+    this.http.post<clock>(this.path + 'clock/alarms/new', clockData);
   }
 
   /*
     Getting the current Song.
   */
   getCurrentSong() {
-    return this.http.get<music>('http://localhost:8080/rest/music/currentSong');
+    return this.http.get<music>(this.path + 'music/currentSong');
   }
 
   /*
     Getting the current Playlist.
   */
   getPlaylist() {
-    return this.http.get<playlist>('http://localhost:8080/rest/music/playlist');
+    return this.http.get<playlist>(this.path + 'music/playlist');
   }
 
   /*
     giving a command to play a song.
   */
   playSong(musicData: music){
-    this.http.post<music>('http://localhost:8080/rest/music/play', musicData);
+    this.http.post<music>(this.path + 'music/play', musicData);
   }
 
   /*
     getting weather-Data for today.
   */
   getWeatherToday() {
-    return this.http.get<weather>('http://localhost:8080/rest/weather/today');
+    return this.http.get<weather>(this.path + 'weather/today');
   }
 
   /*
     getting weather-Data for tomorrow.
   */
   getWeatherTomorrow() {
-  return this.http.get<weather>('http://localhost:8080/rest/weather/tomorrow');
+  return this.http.get<weather>(this.path + 'weather/tomorrow');
   }
 
   /*
     getting weather-Data for the whole week.
   */
   getWeatherWeek() {
-  return this.http.get<weatherWeek>('http://localhost:8080/rest/weather/week');
+  return this.http.get<weatherWeek>(this.path + 'weather/week');
   }
 }
 
