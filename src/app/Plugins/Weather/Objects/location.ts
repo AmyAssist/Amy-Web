cb10f13ffe7080b1cdfb564d2a2c45c8eafc1ca1
Felix B <felix.burk@googlemail.com>
2018-07-18 17:06:05 +0200

    forgot file

--- src/app/Plugins/Weather/Objects/location.ts
+++ src/app/Plugins/Weather/Objects/location.ts
@@ -1,15 +1,15 @@
 export class Location {
-    
-    id : number;
-    name : string;
-    zipCode : string;
-    city : string;
-    street : string;
-    houseNumber : number;
-    longitude : number;
-    latitude : number;
-    isHome : boolean;
-    isWork : boolean;
+
+    id: number;
+    name: string;
+    zipCode: string;
+    city: string;
+    street: string;
+    houseNumber: number;
+    longitude: number;
+    latitude: number;
+    isHome: boolean;
+    isWork: boolean;
 
     constructor() { }
 }
