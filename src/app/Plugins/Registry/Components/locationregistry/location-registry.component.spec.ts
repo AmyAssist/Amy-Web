6be4595b5e59af30e08bec541e06ebba151147b4
Benno <benno@bensge.com>
2018-07-19 20:39:16 +0200

    Add necessary imports in test

--- src/app/Plugins/Registry/Components/locationregistry/location-registry.component.spec.ts
+++ src/app/Plugins/Registry/Components/locationregistry/location-registry.component.spec.ts
@@ -1,6 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationRegistryComponent } from './location-registry.component';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatListModule,
+    MatMenuModule,
+    MatTableModule, MatToolbarModule
+} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTabsModule} from '@angular/material/tabs';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('LocationRegistryComponent', () => {
   let component: LocationRegistryComponent;
@@ -8,7 +29,33 @@ describe('LocationRegistryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationRegistryComponent ]
+      declarations: [ LocationRegistryComponent ],
+      providers: [HttpClientModule],
+      imports: [
+          BrowserModule,
+          MatFormFieldModule,
+          MatButtonModule,
+          MatInputModule,
+          MatCardModule,
+          MatExpansionModule,
+          MatIconModule,
+          MatTabsModule,
+          MatSliderModule,
+          FormsModule,
+          BrowserAnimationsModule,
+          FormsModule,
+          MatSelectModule,
+          MatCheckboxModule,
+          MatToolbarModule,
+          MatMenuModule,
+          MatListModule,
+          MatGridListModule,
+          MatDividerModule,
+          MatIconModule,
+          MatTableModule,
+          ReactiveFormsModule,
+          HttpClientModule
+      ]
     })
     .compileComponents();
   }));
