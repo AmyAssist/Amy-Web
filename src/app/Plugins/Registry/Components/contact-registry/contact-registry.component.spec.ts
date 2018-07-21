be7b103fb4fce2f92b7eacad95f98954ba5dd8b7
Benno <benno@bensge.com>
2018-07-21 20:21:27 +0200

    Fix tests

--- src/app/Plugins/Registry/Components/contact-registry/contact-registry.component.spec.ts
+++ src/app/Plugins/Registry/Components/contact-registry/contact-registry.component.spec.ts
@@ -1,6 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactRegistryComponent } from './contact-registry.component';
+import {
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule, MatDividerModule,
+    MatGridListModule,
+    MatListModule, MatMenuModule,
+    MatTableModule,
+    MatToolbarModule
+} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
 
 describe('ContactRegistryComponent', () => {
   let component: ContactRegistryComponent;
@@ -8,7 +30,34 @@ describe('ContactRegistryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContactRegistryComponent ]
+      declarations: [ ContactRegistryComponent ],
+      imports: [
+        BrowserModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+        MatCardModule,
+        MatExpansionModule,
+        MatIconModule,
+        MatTabsModule,
+        MatSliderModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatListModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatIconModule,
+        MatTableModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+      ]
     })
     .compileComponents();
   }));
