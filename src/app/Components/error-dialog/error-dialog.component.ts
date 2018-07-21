6c0127069f406812901d5558e555a3b14ad07f8c
Benno <benno@bensge.com>
2018-07-21 13:46:56 +0200

    Add error dialog

---
+++ src/app/Components/error-dialog/error-dialog.component.ts
@@ -0,0 +1,27 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+/**
+ * Simple error dialog
+ *
+ * @author Benno Krauß
+ */
+@Component({
+  selector: 'app-error-dialog',
+  templateUrl: './error-dialog.component.html',
+  styleUrls: ['./error-dialog.component.css']
+})
+export class ErrorDialogComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+  constructor(private dialogRef: MatDialogRef<ErrorDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+
+  }
+
+  public closeDialog() {
+      this.dialogRef.close();
+  }
+
+}
