2d6608453bfbb0659854d2a00eb5787f706ba074
Benno <benno@bensge.com>
2018-07-22 12:37:05 +0200

    Optimized imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {
-    MatButtonModule,
     MatCheckboxModule,
-    MatToolbarModule,
-    MatGridListModule,
-    MatMenuModule,
-    MatListModule,
+    MatDialogModule,
     MatDividerModule,
+    MatGridListModule,
+    MatListModule,
+    MatMenuModule,
     MatTableModule,
-    MatDialogModule, MatTabsModule
+    MatTabsModule,
+    MatToolbarModule
 } from '@angular/material';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { ClockComponent } from './Plugins/Clock/Components/clock/clock.component';
-import { WeatherComponent } from './Plugins/Weather/Components/weather/weather.component';
-import { HomeComponent } from './Components/home/home.component';
-import { CalendarComponent } from './Plugins/Calendar/Components/calendar/calendar.component';
-import { NavigationComponent } from './Plugins/Navigation/Components/navigation/navigation.component';
-import { LocationRegistryComponent } from './Plugins/Registry/Components/location-registry/location-registry.component';
-import { ErrorDialogComponent } from './Components/error-dialog/error-dialog.component';
-import { RegistryContainerComponent } from './Plugins/Registry/Components/registry-container/registry-container.component';
-import { ContactRegistryComponent } from './Plugins/Registry/Components/contact-registry/contact-registry.component';
+import {AppComponent} from './app.component';
+import {ClockComponent} from './Plugins/Clock/Components/clock/clock.component';
+import {WeatherComponent} from './Plugins/Weather/Components/weather/weather.component';
+import {HomeComponent} from './Components/home/home.component';
+import {CalendarComponent} from './Plugins/Calendar/Components/calendar/calendar.component';
+import {NavigationComponent} from './Plugins/Navigation/Components/navigation/navigation.component';
+import {LocationRegistryComponent} from './Plugins/Registry/Components/location-registry/location-registry.component';
+import {ErrorDialogComponent} from './Components/error-dialog/error-dialog.component';
+import {RegistryContainerComponent} from './Plugins/Registry/Components/registry-container/registry-container.component';
+import {ContactRegistryComponent} from './Plugins/Registry/Components/contact-registry/contact-registry.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MaterialModule } from './material.module';
-import { MusicModule } from './Plugins/Music/music.module';
+import {MaterialModule} from './material.module';
+import {MusicModule} from './Plugins/Music/music.module';
 
 /*
     Routing of the components to the respective links
