c3d32cecb7aa42f018f8aa7b0529eb49dc96d3f7
Benno <benno@bensge.com>
2018-07-23 12:09:29 +0200

    Return actual error instead of generic error message (if possible)
    This means that the actual error message is passed to the observer. The
    observer can e.x. show the error message to the user.
    Concrete example: If you already have a home-entity and try to set
    another entity to ‘ home’, then the server returns an appropriate error
    message that should be shown to the user

--- src/app/Plugins/Registry/Services/location-registry-data.service.ts
+++ src/app/Plugins/Registry/Services/location-registry-data.service.ts
@@ -48,6 +48,7 @@ export class LocationRegistryDataService {
             console.error(
                 `Backend returned code ${error.status}, ` +
                 `body was: ${error.error}`);
+            return throwError('Error: ' + error.error.toString());
         }
         // return an observable with a user-facing error message
         return throwError('Something bad happened; please try again later.');
