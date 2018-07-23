7dd7a32a8eb2a81c90f16117e1709530872278da
Benno <benno@bensge.com>
2018-07-23 12:45:19 +0200

    Rename error object

--- src/app/Plugins/Registry/Services/location-registry-data.service.ts
+++ src/app/Plugins/Registry/Services/location-registry-data.service.ts
@@ -38,17 +38,17 @@ export class LocationRegistryDataService {
     }
 
 
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
+    private handleError(errorResponse: HttpErrorResponse) {
+        if (errorResponse.error instanceof ErrorEvent) {
             // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
+            console.error('An error occurred:', errorResponse.error.message);
         } else {
             // The backend returned an unsuccessful response code.
             // The response body may contain clues as to what went wrong,
             console.error(
-                `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
-            return throwError('Error: ' + error.error.toString());
+                `Backend returned code ${errorResponse.status}, ` +
+                `body was: ${errorResponse.error}`);
+            return throwError('Error: ' + errorResponse.error.toString());
         }
         // return an observable with a user-facing error message
         return throwError('Something bad happened; please try again later.');
