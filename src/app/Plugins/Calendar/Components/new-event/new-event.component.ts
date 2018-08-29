ecce63773f55971a0515565785e1bbbed24b94a9
Florian Bauer <ba_flo@web.de>
2018-08-29 22:16:32 +0200

    updates new Event form

--- src/app/Plugins/Calendar/Components/new-event/new-event.component.ts
+++ src/app/Plugins/Calendar/Components/new-event/new-event.component.ts
@@ -16,61 +16,103 @@ import { LocalDateTime } from '../../../../Objects/LocalDateTime';
 })
 export class NewEventComponent implements OnInit {
 
+  title: string;
+  description: string;
   allDay: boolean;
+  address: string;
+  city: string;
+  country: string;
+  postalCode: string;
   location: string;
   reminderTime: number;
   timeUnit: string;
   reminderType: string;
-  title: string;
 
   constructor(private calendarService: CalendarDataService) { }
 
   ngOnInit() {
     this.allDay = false;
-    this.location = '';
-    this.reminderTime = null;
-    this.timeUnit = 'minutes';
-    this.reminderType = 'email';
-    this.title = '';
+    this.resetValues();
   }
 
   setAllDay() {
     this.allDay = !this.allDay;
   }
 
-  createEvent(startDateInput, endDateInput, description, address, city, country, postalCode, timeValue): void {
-    console.log(this.title);
-    this.location = address;
+  createEvent(startDateInput, endDateInput, timeValue): void {
+    this.createLocation();
     const startDate = new Date(startDateInput);
     const endDate = new Date(endDateInput);
-    if(this.allDay){
+    if (this.allDay) {
       endDate.setDate(endDate.getDate() + 1);
     }
     const start = new LocalDateTime(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), 0, 0);
     const end = new LocalDateTime(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), 23, 59);
-    if (postalCode !== '') {
-      this.location += ', ' + postalCode;
-      if (city !== '') {
-        this.location += ' ' + city;
-      }
-    } else if (city !== '') {
-      this.location += ', ' + city;
-    }
-    if (country !== '') {
-      this.location += ', ' + country;
-    }
-
-    if (this.timeUnit == 'minutes') {
+    if (this.timeUnit === 'minutes') {
       this.reminderTime = timeValue;
-    } else if (this.timeUnit == 'hours') {
+    } else if (this.timeUnit === 'hours') {
       this.reminderTime = 60 * timeValue;
-    } else if (this.timeUnit == 'days') {
+    } else if (this.timeUnit === 'days') {
       this.reminderTime = 24 * 60 * timeValue;
-    } else if (this.timeUnit == 'weeks') {
+    } else if (this.timeUnit === 'weeks') {
       this.reminderTime = 7 * 24 * 60 * timeValue;
     }
-    const newEvent = CalendarEvent.setEventData(this.title, start.toString(), end.toString(), description, this.location, this.reminderType, this.reminderTime, '', this.allDay);
+    const newEvent = CalendarEvent.setEventData(this.title, start.toString(), end.toString(), this.description, this.location, this.reminderType, this.reminderTime, '', this.allDay);
     this.calendarService.setNewEvent(newEvent).subscribe();
+    this.resetValues();
+  }
+
+  resetValues(): void {
+    this.location = '';
+    this.reminderTime = null;
+    this.timeUnit = 'minutes';
+    this.reminderType = 'email';
+    this.title = '';
+    this.description = '';
+    this.address = '';
+    this.city = '';
+    this.postalCode = '';
+    this.country = '';
+  }
+
+  // this method makes sure that the location is displayed the right way
+  createLocation(): void {
+    this.location += this.address;
+    if (this.address === '' && this.postalCode === '' && this.city === '') {
+      this.location += this.country;
+    } else if (this.address === '' && this.postalCode === '') {
+      this.location += this.city;
+      if (this.country !== '') {
+        this.location += ', ' + this.country;
+      }
+    } else if (this.address === '') {
+      this.location += this.postalCode;
+      if (this.city !== '') {
+        this.location += ' ' + this.city;
+        if (this.country !== '') {
+          this.location += ', ' + this.country;
+        }
+      } else if (this.country !== '') {
+        this.location += ', ' + this.country;
+      }
+    } else {
+      if (this.postalCode !== '') {
+        this.location += ', ' + this.postalCode
+        if (this.city !== '') {
+          this.location += ' ' + this.city;
+        }
+        if (this.country !== '') {
+          this.location += ', ' + this.country;
+        }
+      } else if (this.city !== '') {
+        this.location += ', ' + this.city;
+        if (this.country !== '') {
+          this.location += ', ' + this.country;
+        }
+      } else if (this.country !== '') {
+        this.location += ', ' + this.country;
+      }
+    }
   }
 
 }
