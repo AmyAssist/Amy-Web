8e00302458220b8a1a93bc1568816efdf1c3cbcb
Florian Bauer <ba_flo@web.de>
2018-09-09 18:36:33 +0200

    refactoring choosen date

--- src/app/Plugins/Calendar/Components/events-day/events-day.component.ts
+++ src/app/Plugins/Calendar/Components/events-day/events-day.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
 import { CalendarEvent } from '../../Objects/CalendarEvent';
 import { CalendarDataService } from '../../Services/calendar-data.service';
 import { LocalDateTime } from '../../../../Objects/LocalDateTime';
+import { MatInput } from '@angular/material';
 
 /*
   Component for viewing events on different dates.
@@ -18,26 +19,31 @@ export class EventsDayComponent implements OnInit {
 
   events: CalendarEvent[];
   onDate: boolean;
+  noDate: boolean;
+  dateChoosen: boolean;
   selectedDate: string;
-  today: boolean;
-  tomorrow: boolean;
   dateString: string;
+  
+  @ViewChild('eventDate', {
+    read: MatInput
+  }) eventDate: MatInput;
 
   constructor(private readonly calendarService: CalendarDataService) { }
 
   ngOnInit() {
     this.events = [];
     this.chosenDate('today');
-    this.today = true;
-    this.tomorrow = false;
   }
 
   public chosenDate(eventDate: string): void {
+    this.noDate = false;
     this.dateString = eventDate;
     if (eventDate === 'today' || eventDate === 'tomorrow') {
       this.getDate(eventDate);
+      this.dateChoosen = false;
       this.onDate = false;
     } else {
+      this.dateChoosen = true;
       const makeDate = new Date(eventDate);
       this.selectedDate = new LocalDateTime(makeDate.getFullYear(), makeDate.getMonth(), makeDate.getDate(), 0, 0).toString();
     }
@@ -45,13 +51,9 @@ export class EventsDayComponent implements OnInit {
   }
 
   public getDate(eventDate: string) {
-    this.today = true;
-    this.tomorrow = false;
     const tzoffset = (new Date()).getTimezoneOffset() * 60000;
     const currentDate = new Date(Date.now() - tzoffset);
     if (eventDate === 'tomorrow') {
-      this.today = false;
-      this.tomorrow = true;
       currentDate.setDate(currentDate.getDate() + 1);
     }
     this.selectedDate = currentDate.toISOString().slice(0, -1);
@@ -64,9 +66,13 @@ export class EventsDayComponent implements OnInit {
   }
 
   public getEventsOnDate(): void {
+    if(this.eventDate) {
+      this.eventDate.value = ``;
+    }
+    this.dateChoosen = false;
+    this.noDate = true;
+    this.dateString = ``;
     this.onDate = true;
-    this.today = false;
-    this.tomorrow = false;
   }
 
   public refresh(): void {
